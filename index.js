734cb03bcdc63f9661ab6c5e59835137c483a7e7
Gagik Arustamyan <g.arustamyan@gmail.com>
2017-08-22 15:46:12 +0400

    Add xmlrpc communication to the app

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-var express = require('express'),
-    http = require('http'),
+const express = require('express'),
     redis = require('redis'),
-    staff = require('./lib/staff');
+    fetchEmployees = require('./lib/fetcher');
 
-var app = express();
+const app = express();
 
-var client = redis.createClient('6379', 'redis');
+const client = redis.createClient('6379', 'redis');
 
-app.get('/api/staff', (req, res, next) => {
-  res.end(JSON.stringify(staff));
+app.get('/api/staff', async (req, res, next) => {
+    try {
+        let employees = await fetchEmployees();
+        res.end(JSON.stringify(employees));
+    } catch (e) {
+        res.end('something went wrong: \n', JSON.stringify(e))
+    }
 });
 
 app.use(express.static('public'));
 
 app.listen(process.env.PORT || 8080, () => {
-  console.log('Listening on port ' + (process.env.PORT || 8080));
+    console.log('Listening on port ' + (process.env.PORT || 8080));
 });
