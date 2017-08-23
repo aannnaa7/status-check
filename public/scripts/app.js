c43705bc8b501d05d862ff4894e5be29923ed510
Vako30 <vardgesvardanyan30@gmail.com>
2017-08-23 16:22:44 +0400

    implemented issues #2 and #4

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,78 +1,168 @@
-var HASH_DELIMITER = '#!';
+var APP = (function () {
+	var model = {
+		accounts:[],
+		controllerData:[],
+		orederOptions:[
+			{
+				title: "Name",
+				value: "fullName"
+			},
+			{
+				title: "Status",
+				value: "lastStatusChange"
+			}
+		],
+		selectedOption: "fullName"
+	};
 
-/**
- * takes a list of nodes that have data-path attributes
- * and builds an array of objects that has path and whenActive properties
- *
- * @param {*} nodes
- */
-function populateRoutesFromDom(nodes) {
-    var routes = Array.prototype.slice.apply(nodes);
-    routes = routes.map(function (route) {
-        return {
-            path: route.dataset.routePath,
-            whenActive: route.dataset.routeWhenActive,
-            searchCriteria: route.dataset.routeSearchCriteria,
-            element: route,
-            default: !!route.dataset.routeDefault
-        }
-    });
-    return routes;
-}
+	var statusController = {
+		init: function(params){
+			view.loader();
 
-/**
- * filters out the route that matches with the current hash
- * and sets the whenActive class
- * passes on control to loadStatus function
- *
- * @param {*} routes
- * @param {*} currentHash
- */
-function activeteRoute(routes, currentHash) {
-    var path = currentHash.replace(HASH_DELIMITER, '');
+			var xhttp = new XMLHttpRequest();
+			xhttp.open("GET", "api/staff", true);
+			xhttp.send();
+			xhttp.onreadystatechange = function() {
+				if (this.readyState == 4 && this.status == 200) {
 
-    if (path) {
-        routes.forEach(function (route) {
-            if (route.path.indexOf(path) !== -1) {
-                route.element.classList.add(route.whenActive);
-                loadStatus(route.searchCriteria);
-            } else {
-                route.element.classList.remove(route.whenActive);
-            }
-        });
-    } else {
-        var defaultRoute = routes.filter(function(route) {
-            return route.default;
-        })[0];
+					model.accounts = JSON.parse(this.responseText);
+					model.controllerData = model.accounts.filter(function(account){
+						return account.status == params.status;
+					});
+					view.render();
+				}
+			};
+		},
 
-        activeteRoute(routes, HASH_DELIMITER + defaultRoute.path);
-    }
-}
+		subscribe: function(id){
+			for(index = 0; index < model.accounts.length; index++){
+				if(model.accounts[index].id == id){
+					model.accounts[index].subscribed = !model.accounts[index].subscribed;
+					break;
+				}
+			}
+			view.render();
+		},
 
-/**
- * call the api to retrieve staff that matches the searchCriteria
- *
- * @param {string} searchCriteria
- */
-function loadStatus(searchCriteria) {
-    console.log('loading status: ', searchCriteria);
-}
+		updateOrdering: function(sortBy){
+			model.selectedOption = sortBy;
 
-function makeGetRequest(url) {
-    //
-}
+			var arr = model.controllerData;
+			model.controllerData.sort(function (a, b) {
+			  return a[sortBy] > b[sortBy] ? 1 : a[sortBy] == b[sortBy] ? 0 : -1;
+			});
+			view.render();
+		},
 
-window.addEventListener('DOMContentLoaded', () => {
+		getOrderOptions: function(){
+			return model.orederOptions;
+		}
+	};
 
-    var routeItems = document.querySelectorAll('[data-route-path]'),
-        routes;
+	var view = {
+		formatDate: function(milliseconds) {
+			var date = new Date(milliseconds);
+			var monthNames = [
+				"January", "February", "March",
+				"April", "May", "June", "July",
+				"August", "September", "October",
+				"November", "December"
+			];
 
-    routes = populateRoutesFromDom(routeItems);
+			var day = date.getDate();
+			var monthIndex = date.getMonth();
+			var year = date.getFullYear();
+			var hours = date.getHours();
+			var minutes = date.getMinutes();
+			var seconds =  date.getSeconds();
 
-    activeteRoute(routes, location.hash);
+			if(hours < 10) hours = "0"+hours;
+			if(minutes < 10) minutes = "0"+minutes;
+			if(seconds < 10) seconds = "0"+seconds;
 
-    window.addEventListener('hashchange', function () {
-        activeteRoute(routes, location.hash);
-    });
+			return day + ' ' + monthNames[monthIndex] + ' ' + year + ' ' + hours + ":"  + minutes + ":"  + seconds;
+		},
 
-});
+		createArticle: function(account){
+			var article = document.createElement('article');
+			article.classList.add("employee");
+
+			var div = document.createElement('div');
+			div.classList.add("employee__wrapper");
+			article.appendChild( div );
+
+			var img = document.createElement('img');
+			img.classList.add("employee__avatar");
+			img.src = "assets/staff/tatev.jpg";
+			div.appendChild(img);
+
+			var h2 = document.createElement('h2');
+			h2.classList.add("employee__name");
+			h2.textContent = account.fullName;
+			div.appendChild(h2);
+
+			var p = document.createElement('p');
+			p.classList.add("employee__date");
+			p.textContent = view.formatDate(account.lastStatusChange);
+			div.appendChild(p);
+
+			var button = document.createElement('button');
+			button.classList.add("employee__subscribe");
+			var icon = account.subscribed ? "checkmark":"bell";
+			button.innerHTML = '<svg class="icon icon-'+icon+'"><use xlink:href="#icon-'+icon+'"></use></svg>';
+			button.addEventListener("click", function(){
+				statusController.subscribe(account.id);
+			});
+			div.appendChild(button);
+
+			return article;
+		},
+
+		createSelect: function(){
+			var select = document.createElement('select');
+
+			var options = statusController.getOrderOptions();
+			for(index = 0; index < options.length; index++){
+				var option = document.createElement('option');
+				option.textContent = options[index].title;
+				option.setAttribute('value', options[index].value);
+				if(model.selectedOption == options[index].value) option.setAttribute('selected','selected');
+				select.appendChild(option);
+			}
+
+			select.addEventListener("change", function(){
+				statusController.updateOrdering(select.options[select.selectedIndex].value);
+			});
+
+			return select;
+		},
+
+		render: function(accounts){
+			main.innerHTML = "";
+
+			var select = view.createSelect();
+			main.appendChild(select);
+
+			var div = document.createElement('div');
+			for(index = 0; index < model.controllerData.length; index++){
+				var article = view.createArticle(model.controllerData[index]);
+				div.appendChild( article );
+			}
+			var clear = document.createElement('div');
+			clear.classList.add("clear");
+			div.appendChild(clear);
+			main.appendChild(div);
+		},
+
+		loader: function(){
+			main.innerHTML = 
+			'<div style="text-align:center"><svg width="200px"  height="200px"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" class="lds-rolling" style="background: none; margin:50px auto">'+
+				'<circle cx="50" cy="50" fill="none" stroke="#fbba1f" stroke-width="10" r="35" stroke-dasharray="164.93361431346415 56.97787143782138" transform="rotate(257.418 50 50)">'+
+					'<animateTransform attributeName="transform" type="rotate" calcMode="linear" values="0 50 50;360 50 50" keyTimes="0;1" dur="1s" begin="0s" repeatCount="indefinite"></animateTransform>'+
+				'</circle>'+
+			'</svg></div>';
+		}
+	};
+
+	return {statusController};
+}());
--- public/sw.js
+++ public/sw.js
@@ -1,51 +1,102 @@
-var CACHE_NAME = 'my-site-cache-v1';
-var urlsToCache = [
+/*
+*
+*  Push Notifications codelab
+*  Copyright 2015 Google Inc. All rights reserved.
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*      https://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License
+*
+*/
+
+/* eslint-env browser, serviceworker, es6 */
+
+'use strict';
+
+self.addEventListener('push', function(event) {
+  console.log('[Service Worker] Push Received.');
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+  const title = 'Push Codelab';
+  const options = {
+    body: event.data.text(),
+    icon: 'images/icon.png',
+    badge: 'images/badge.png'
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+var cacheName = 'weatherPWA-step-6-1';
+var filesToCache = [
   '/',
-  '/styles/barcamp.css',
-  '/script/barcamp.js'
+  '/index.html',
+  '/styles/style.html',
+  '/scripts/app.js'
 ];
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // The resource is found in the cache
-        if (response) {
-          return response;
-        }
-        // Otherwise go to network
-        return fetch(event.request);
-      }
-    )
-  );
-});
-
-self.addEventListener('activate', function(event) {
-
-  var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
-
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
     })
   );
 });
 
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
 
+self.addEventListener('fetch', function(e) {
+  console.log('[Service Worker] Fetch', e.request.url);
+  var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
+  if (e.request.url.indexOf(dataUrl) > -1) {
+    /*
+     * When the request URL contains dataUrl, the app is asking for fresh
+     * weather data. In this case, the service worker always goes to the
+     * network and then caches the response. This is called the "Cache then
+     * network" strategy:
+     * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
+     */
+    e.respondWith(
+      caches.open(dataCacheName).then(function(cache) {
+        return fetch(e.request).then(function(response){
+          cache.put(e.request.url, response.clone());
+          return response;
+        });
+      })
+    );
+  } else {
+    /*
+     * The app is asking for app shell files. In this scenario the app uses the
+     * "Cache, falling back to the network" offline strategy:
+     * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
+     */
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+    );
+  }
+});
